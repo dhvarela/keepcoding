e9cb5e8e06c6bd737bc9e64af6a9e5d31424c550
Daniel <daniloviciv@hotmail.com>
2017-06-15 21:32:10 +0200

    different requests (get, post) and different routes defined

---
+++ express_generator2/generated/routes/clients.js
@@ -0,0 +1,25 @@
+/**
+ * Created by daniel on 15/06/17.
+ */
+
+"use strict";
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    console.log('req.query: '+req.query.name);
+    res.send('clients response');
+});
+
+router.get('/:id', function(req, res, next) {
+    console.log(req.params);
+    res.send('parameter ' +req.params.id + ' received');
+});
+
+router.post('/', function(req, res, next){
+    console.log('req.body: ' + req.body.num);
+    res.json({received: req.body.num });
+});
+
+module.exports = router;
